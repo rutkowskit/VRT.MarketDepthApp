52127bc7eb4ece49e9e10ddecb9ebb3d756d8639
rutkowskit <rutkowskit@gmail.com>
2023-12-29 17:18:16 +0100

    Extend market selection user control to handle enter key and arrows

--- VRT.MarketDepth.Wpf/Controls/SelectMarketUserControl.xaml.cs
+++ VRT.MarketDepth.Wpf/Controls/SelectMarketUserControl.xaml.cs
@@ -1,9 +1,7 @@
-﻿using CommunityToolkit.Mvvm.Input;
-using DependencyPropertyGenerator;
+﻿using DependencyPropertyGenerator;
 using MaterialDesignThemes.Wpf;
 using System.Windows.Controls;
 using System.Windows.Input;
-using VRT.MarketDepth.Services;
 
 namespace VRT.MarketDepth.Wpf.Controls;
 
@@ -40,21 +38,72 @@ public partial class SelectMarketUserControl : UserControl
     partial void OnMarketsChanged(GetMarketsResponseItem[]? newValue)
     {
         _allMarkets = newValue ?? [];
-        FilteredMarkets = FilterMarkets(_allMarkets).ToArray();
+        ApplyMarketsFiltering();
     }
     partial void OnFilterTextChanged(string? newValue)
     {
-        FilteredMarkets = FilterMarkets(_allMarkets).ToArray();
+        ApplyMarketsFiltering();
     }
-    private GetMarketsResponseItem[] FilterMarkets(GetMarketsResponseItem[] items)
+    private void ApplyMarketsFiltering()
     {
         var parts = FilterText?.Split() ?? [];
-        if (parts.Length == 0)
+        var items = _allMarkets;
+        if (parts.Length > 0)
         {
-            return items;
+            items = items
+                .Where(i => parts.All(p => i.ToString().Contains(p, StringComparison.InvariantCultureIgnoreCase)))
+                .ToArray();
         }
-        return items
-            .Where(i => parts.All(p => i.ToString().Contains(p, StringComparison.InvariantCultureIgnoreCase)))
-            .ToArray();
+        FilteredMarkets = items;
+        SelectedMarket = items.FirstOrDefault();
+    }
+
+    private void OnMarketDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        if (CanConfirmSelection())
+        {
+            ConfirmSelectionCommand.Execute(null);
+        }
+    }
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            if (CanConfirmSelection())
+            {
+                ConfirmSelectionCommand.Execute(null);
+            }
+        }
+        base.OnKeyDown(e);
+    }
+
+    private void OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (FilteredMarkets is null)
+        {
+            return;
+        }
+        var markets = FilteredMarkets!;
+        if (e.Key == Key.Down)
+        {
+            var nextIndex = Array.IndexOf(markets, SelectedMarket) + 1;
+            if (nextIndex >= markets.Length || nextIndex < 0)
+            {
+                nextIndex = Math.Max(0, markets.Length - 1);
+            }
+            SelectMarketAt(nextIndex);
+            e.Handled = true;
+        }
+        else if (e.Key == Key.Up)
+        {
+            var nextIndex = Math.Max(0, Array.IndexOf(markets, SelectedMarket) - 1);
+            SelectMarketAt(nextIndex);
+            e.Handled = true;
+        }
+    }
+    private void SelectMarketAt(int index)
+    {
+        SelectedMarket = FilteredMarkets?.ElementAtOrDefault(index);
+        uxMarketsList.ScrollIntoView(SelectedMarket);
     }
 }
